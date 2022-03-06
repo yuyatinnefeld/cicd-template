fbde9ad6ce861eed8fa75ca11a2cc1507bb2f29f
yuyatinnefeld <yuyawamo@gmail.com>
2022-03-06 19:34:33 +0100

    fix: unittest, pylint, flake8

--- app/main.py
+++ app/main.py
@@ -1,16 +1,13 @@
-from typing import Optional
-
+"""FastAPI Backend """
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
 from app.skills import language, cloud, devops
 
 
-description = """Resume Backend Service API 🚀"""
-
 app = FastAPI(
     title="Resume Backend API",
-    description=description,
+    description="Resume Backend Service API 🚀",
     version="0.0.1",
     contact={
         "name": "Yuya Tinnefeld",
@@ -21,23 +18,29 @@ app = FastAPI(
         "name": "Visit Github",
         "url": "https://github.com/yuyatinnefeld",
     },
-    
 )
 
 
 @app.get("/")
 def read_root():
+    """Home Endpoint"""
     item = {"msg": "FastAPI World"}
     return JSONResponse(content=item)
 
+
 @app.get("/language")
 async def read_language():
+    """Language Endpoint"""
     return JSONResponse(content=language)
+
 
 @app.get("/cloud")
 async def read_cloud():
+    """Cloud Endpoint"""
     return JSONResponse(content=cloud)
+
 
 @app.get("/devops")
 async def read_devops():
+    """DevOps Endpoint"""
     return JSONResponse(content=devops)
